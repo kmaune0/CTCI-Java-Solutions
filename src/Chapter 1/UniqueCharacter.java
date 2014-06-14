f8542e75ad5b26abbeaa5b750980ccd1ad8ca0dc
kmaune0 <kmaune71@gmail.com>
2014-06-13 17:51:09 -0700

    fixed errors in comments

--- src/Chapter 1/UniqueCharacter.java
+++ src/Chapter 1/UniqueCharacter.java
@@ -1,7 +1,7 @@
 // UniqueCharacter.java
 // (C) 2014 kmaune0
 // Implement an algorithm to determine if a string has all unique
-// characters.
+// characters. Problem 1.1
 
 import java.util.HashSet;
 
