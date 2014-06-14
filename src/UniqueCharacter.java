1aaf77075745482bedcb5d58a2bbd37f06daa73f
kmaune0 <kmaune71@gmail.com>
2014-06-13 17:46:29 -0700

    added Chap. 1.1 solution
    Implement an algorithm to determine if a string has all unique
    characters.

---
+++ src/UniqueCharacter.java
@@ -0,0 +1,26 @@
+// UniqueCharacter.java
+// (C) 2014 kmaune0
+// Implement an algorithm to determine if a string has all unique
+// characters.
+
+import java.util.HashSet;
+
+public class UniqueCharacter {
+
+  public static boolean isCharacterUnique(String str) {
+    HashSet<Character> charCount = new HashSet<Character>();
+
+    for(int i = 0; i < str.length(); i++) {
+      if(charCount.contains(str.charAt(i))) {
+        return false;
+      }
+
+      else {
+        charCount.add(str.charAt(i));
+
+      }
+    }
+    return true;
+
+  }
+}
